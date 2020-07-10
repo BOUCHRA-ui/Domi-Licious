48c3c52e9ef50adfbc9e36d748a60582d149e4a5
Bouchra <bouchra.trabelsi@gmail.com>
2020-07-10 16:25:59 +0200

    page commentaire
    mise en pag de la page commentaire et debut du calendrier

--- src/DataFixtures/MenuFixture.php
+++ src/DataFixtures/MenuFixture.php
@@ -24,7 +24,10 @@ class MenuFixture extends Fixture implements DependentFixtureInterface
     public function load(ObjectManager $manager)
     {
             $chef = $this->getReference('chef');
+
             $typeCuisine = $this->getReference('type-cuisine');
+
+            
 
     
             $menu = new Menu();
@@ -45,51 +48,51 @@ class MenuFixture extends Fixture implements DependentFixtureInterface
             $menu2->setImage('s2.jpg');
             $menu2->setTypeCuisine($typeCuisine);
             $menu2->setChef($chef);
-            $menu->setPrice(200);
+            $menu2->setPrice(200);
 
             $manager->persist($menu);
 
-            $menu = new Menu();
-            $menu->setEntree('huitres');
-            $menu->setPlat('suamon au riz');
-            $menu->setDessert('cremme brulée');
-            $menu->setImage('s3.jpg');
-            $menu->setTypeCuisine($typeCuisine);
-            $menu->setChef($chef);
-            $menu->setPrice(200);
+            $menu3 = new Menu();
+            $menu3->setEntree('huitres');
+            $menu3->setPlat('suamon au riz');
+            $menu3->setDessert('cremme brulée');
+            $menu3->setImage('s3.jpg');
+            $menu3->setTypeCuisine($typeCuisine);
+            $menu3->setChef($chef);
+            $menu3->setPrice(200);
 
             $manager->persist($menu);
 
-            $menu = new Menu();
-            $menu->setEntree('salade de tomate et mozzarella');
-            $menu->setPlat('chou farci au crevette');
-            $menu->setDessert('foret noir');
-            $menu->setImage('s4.jpg');
-            $menu->setTypeCuisine($typeCuisine);
-            $menu->setChef($chef);
-            $menu->setPrice(200);
+            $menu4 = new Menu();
+            $menu4->setEntree('salade de tomate et mozzarella');
+            $menu4->setPlat('chou farci au crevette');
+            $menu4->setDessert('foret noir');
+            $menu4->setImage('s4.jpg');
+            $menu4->setTypeCuisine($typeCuisine);
+            $menu4->setChef($chef);
+            $menu4->setPrice(200);
 
             $manager->persist($menu);
 
-            $menu = new Menu();
-            $menu->setEntree('tapas');
-            $menu->setPlat('pizza 4 fromage');
-            $menu->setDessert('tarte au myrtille');
-            $menu->setImage('s5.jpg');
-            $menu->setTypeCuisine($typeCuisine);
-            $menu->setChef($chef);
-            $menu->setPrice(200);
+            $menu5 = new Menu();
+            $menu5->setEntree('tapas');
+            $menu5->setPlat('pizza 4 fromage');
+            $menu5->setDessert('tarte au myrtille');
+            $menu5->setImage('s5.jpg');
+            $menu5->setTypeCuisine($typeCuisine);
+            $menu5->setChef($chef);
+            $menu5->setPrice(200);
 
             $manager->persist($menu);
 
-            $menu = new Menu();
-            $menu->setEntree('bacon au melon');
-            $menu->setPlat('poulet roti');
-            $menu->setDessert('tiramisu');
-            $menu->setImage('s6.jpg');
-            $menu->setTypeCuisine($typeCuisine);
-            $menu->setChef($chef);
-            $menu->setPrice(200);
+            $menu6 = new Menu();
+            $menu6->setEntree('bacon au melon');
+            $menu6->setPlat('poulet roti');
+            $menu6->setDessert('tiramisu');
+            $menu6->setImage('s6.jpg');
+            $menu6->setTypeCuisine($typeCuisine);
+            $menu6->setChef($chef);
+            $menu6->setPrice(200);
 
             $manager->persist($menu);
      
